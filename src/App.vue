32bacd02deabcc8075776c139680bef19307f957
Zaid Iqbal <zaid.iqbal@teknuk.com>
2019-07-02 19:56:39 +0500

    commented user component

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,8 @@
         <app-blue></app-blue>
         <app-green></app-green>
         <app-red></app-red>
-        <app-user></app-user>
+        <!-- <app-user></app-user> -->
+        <app-gist></app-gist>
       </div>
     </div>
   </div>
@@ -23,6 +24,7 @@
   import Green from './components/Green';
   import Red from './components/Red';
   import User from './components/User';
+  import Gist from './components/Gist';
   export default {
     name: 'app',
     data () {
@@ -35,6 +37,7 @@
       appGreen: Green,
       appRed: Red,
       appUser: User,
+      appGist: Gist,
     }
   }
 </script>
