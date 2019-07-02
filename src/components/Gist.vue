faa31a486b8f937fdb251181fa42fa7ce67b1951
Zaid Iqbal <zaid.iqbal@teknuk.com>
2019-07-02 19:48:37 +0500

    created gist component and retreived single gist object

---
+++ src/components/Gist.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="form-group">
+    <button class="btn btn-primary" @click="fetchGist">Get Gist</button>
+    <br><br>
+    <p>{{gist}}</p>
+    </div>
+  </div> 
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      gist:'',
+    }
+  },
+  created(){
+    this.fetchGist();
+  },
+  methods: {
+    fetchGist() {
+      this.$http.get('https://api.github.com/gists')
+          .then(response => {
+            this.gist = response.data[0]
+            return response.json();
+          })
+    }
+  }    
+}
+</script>
+
+<style>
+
+</style>
