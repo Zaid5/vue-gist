22fc586b43605ac95e88b56c02540ef163c5a2db
Zaid Iqbal <zaid.iqbal@teknuk.com>
2019-07-02 18:33:25 +0500

    created component of user

--- src/App.vue
+++ src/App.vue
@@ -12,24 +12,7 @@
         <app-blue></app-blue>
         <app-green></app-green>
         <app-red></app-red>
-        <div class="form-group">
-          <label> Username</label>
-          <input type="text" class="form-control" v-model="user.username">
-        </div>
-        <div class="form-group">
-          <label> Email</label>
-          <input type="text" class="form-control" v-model="user.email">
-        </div>
-        <button class="btn btn-primary" @click="submit">Submit</button>
-        <hr>
-        <hr>
-        <button class="btn btn-primary" @click="fetchData">Get Data</button>
-        <br><br>
-        <ul class="list-group">
-          <li class="list-group-item" v-for="u in users">
-            {{u.username}} - {{u.email}}
-          </li>
-        </ul>
+        <app-user></app-user>
       </div>
     </div>
   </div>
@@ -39,46 +22,19 @@
   import Blue from './components/Blue';
   import Green from './components/Green';
   import Red from './components/Red';
+  import User from './components/User';
   export default {
     name: 'app',
     data () {
       return {
         msg: 'Welcome to Your Vue.js App',
-        user:{
-          username:'',
-          email:'',
-        },
-        users:[],
       }
     },
     components:{
       appBlue: Blue,
       appGreen: Green,
       appRed: Red,
-    },
-    methods: {
-      submit() {
-        this.$http.post('', this.user)
-            .then(response => {
-              console.log(response);
-            }, error => {
-              console.log(error);
-            });
-      },
-      fetchData() {
-        this.$http.get('')
-            .then(response => {
-              return response.json();
-
-            })
-            .then(data => {
-              const resultArray = [];
-              for (let key in data) {
-                resultArray.push(data[key]);
-              }
-              this.users = resultArray;
-            });
-      }
+      appUser: User,
     }
   }
 </script>
---
+++ src/components/User.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div class="form-group">
+    <label> Username</label>
+    <input type="text" class="form-control" v-model="user.username">
+    </div>
+    <div class="form-group">
+      <label> Email</label>
+      <input type="text" class="form-control" v-model="user.email">
+    </div>
+    <button class="btn btn-primary" @click="submit">Submit</button>
+    <hr>
+    <hr>
+    <button class="btn btn-primary" @click="fetchData">Get Data</button>
+    <br><br>
+    <ul class="list-group">
+      <li class="list-group-item" v-for="u in users">
+        {{u.username}} - {{u.email}}
+      </li>
+    </ul>
+  </div> 
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      user:{
+        username:'',
+        email:'',
+      },
+      users:[],
+    }
+  },
+  methods: {
+    submit() {
+      this.$http.post('', this.user)
+          .then(response => {
+            console.log(response);
+          }, error => {
+            console.log(error);
+          });
+    },
+    fetchData() {
+      this.$http.get('')
+          .then(response => {
+            return response.json();
+
+          })
+          .then(data => {
+            const resultArray = [];
+            for (let key in data) {
+              resultArray.push(data[key]);
+            }
+            this.users = resultArray;
+          });
+    }
+  }    
+}
+</script>
+
+<style>
+
+</style>
