648acea3ed4f327093972da9b45496c57d8fc388
Zaid Iqbal <zaid.iqbal@teknuk.com>
2019-07-08 20:28:14 +0500

    configuring routes

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import { routes } from "./routes"
 
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes,    //router: router
+  mode: 'history' 
+});
 Vue.use(VueResource);
 Vue.http.options.root = 'https://vue-gist.firebaseio.com/data.json';
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
---
+++ src/routes.js
@@ -0,0 +1,6 @@
+import NewGist from './components/NewGist.vue';
+export const routes = [{
+    path: '',
+    component: NewGist
+  },
+];
