a41a21f05cddc7c71012d3fdbaddeebb58203e94
Zaid Iqbal <zaid.iqbal@teknuk.com>
2019-07-12 18:35:31 +0500

    added login and signup components for auth

---
+++ src/components/Login.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="login">
+    <div class="card text">
+      <div class="card-header">
+        <h3>Sign In to View Gists</h3>
+      </div>
+        <div class="card-body text">
+          <div class="form-group">
+            <label>Email address</label>
+            <input 
+              type="text" 
+              v-model="email" 
+              placeholder="Enter email" 
+              class="form-control" 
+              required>
+          </div>
+          <div class="form-group">
+            <label>Password</label>
+            <input 
+              type="password" 
+              v-model="password"
+              placeholder="Password" 
+              class="form-control" 
+              required>
+          </div>
+        </div>
+        <button v-on:click="login" class="btn btn-primary">Sign In</button>
+        <p><router-link to="/sign-up">
+          New Here? Create a new account
+        </router-link></p>
+    </div>
+  </div>
+</template>
+
+<script>
+  import firebase from 'firebase'
+
+  export default {
+    name: 'login',
+    data: function() {
+      return {
+        email: '',
+        password: ''
+      }
+    },
+    methods: {
+      login () {
+        firebase.auth().signInWithEmailAndPassword(this.email, this.password).then((user) => {
+          this.$router.replace('/allgists')
+        }).catch((err) => {
+          alert(err.message)
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.login {
+  margin-top: 40px;
+}
+input {
+  align-self: center;
+  margin: 10px 0;
+  width: auto;
+  padding: 15px;
+}
+button {
+  margin: 10px 0;
+}
+a {
+  color: black;
+  text-decoration: underline;
+}
+
+p {
+  margin-top: 40px;
+  font-size: 13px;
+}
+h1, h2 {
+  font-weight: normal;
+}
+</style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="sign-up">
+    <div class="card">
+      <div class="card-header">
+        <h3>Create a new account</h3>
+      </div>
+        <div class="card-body" align='center'>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" 
+                  v-model="email"
+                  class="form-control" 
+                  id="exampleInputEmail1" 
+                  aria-describedby="emailHelp" 
+                  placeholder="Enter email"
+                  required>
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" 
+                  v-model="password"
+                  class="form-control" 
+                  id="exampleInputPassword1" 
+                  placeholder="Password"
+                  required>
+          </div>
+        </div>
+      <button v-on:click="signUp" class="btn btn-primary">Sign Up!</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  import firebase from 'firebase'
+  export default {
+    name: 'signup',
+    data () {
+      return {
+        email: '',
+        password: ''
+      }
+    },
+    methods: {
+      signUp () {
+        firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then((user) => {
+          this.$router.replace('/login')
+        }).catch((err) => {
+          alert(err.message)
+        });
+      },
+    }
+  }
+</script>
+
+<style scoped>
+.sign-up {
+  margin-top: 40px;
+}
+input {
+  align-self: center;
+  margin: 10px 0;
+  width: auto;
+  padding: 15px;
+}
+button {
+  margin: 10px 0;
+}
+a {
+  color: #2c3e50;
+}
+p {
+  margin-top: 40px;
+  font-size: 13px;
+}
+h3 {
+  font-weight: normal;
+}
+</style>
