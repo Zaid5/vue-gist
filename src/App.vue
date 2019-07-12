b45993bf30e1e31935177b36d448d3e6f0756de3
Zaid Iqbal <zaid.iqbal@teknuk.com>
2019-07-12 18:39:52 +0500

    implemented login and logout functionality and removed some extra components

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,11 @@
       <img src="./assets/logo.png" height="50">
       <h1 align="center">{{ msg }}</h1>
       <hr>
-      <div class="col-xs-12">
-        <button class="btn btn-primary">Click for Blue Component</button>
-        <button class="btn btn-success">Click for Green Component</button>
-        <button class="btn btn-danger">Click for Red Component</button>
-        <hr>
-        <app-blue></app-blue>
-        <app-green></app-green>
-        <app-red></app-red>
-        <!-- <app-user></app-user> -->
-        <!-- <app-gist></app-gist> -->
-        <!-- <app-new-gist></app-new-gist> -->
+      <div class='pull-right'>
+        <button class='btn btn-info' @click="logout">Logout</button>
+        <button class='btn btn-info' @click="logout">Login</button>
+      </div>
+      <div class="col-xs-12" align='center'>
         <router-view></router-view>
       </div>
     </div>
@@ -28,6 +22,10 @@
   import User from './components/User';
   import Gist from './components/Gist';
   import NewGist from './components/NewGist';
+  import Login from './components/Login';
+  import SignUp from './components/SignUp';
+  import firebase from 'firebase';
+
   export default {
     name: 'app',
     data () {
@@ -41,7 +39,16 @@
       appRed: Red,
       appUser: User,
       appGist: Gist,
-      appNewGist: NewGist
+      appNewGist: NewGist,
+      appLogin: Login,
+      appSignUp: SignUp,
+    },
+    methods: {
+      logout: function() {
+        firebase.auth().signOut().then(() => {
+          this.$router.replace('/login')
+        })
+      },
     }
   }
 </script>
