03c331b69cd8a59d1227e2db5db92c35a7cec137
Zaid Iqbal <zaid.iqbal@teknuk.com>
2019-07-12 18:33:49 +0500

    configured firebase with app for authentication

--- src/main.js
+++ src/main.js
@@ -3,17 +3,36 @@ import VueResource from 'vue-resource'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import { routes } from "./routes"
+import * as firebase from 'firebase'
+
 
 Vue.use(VueRouter);
+Vue.use(firebase);
+
+
+Vue.config.productionTip = false
+
 
 const router = new VueRouter({
     routes, //router: router
     mode: 'history'
 });
+
 Vue.use(VueResource);
 // Vue.http.options.root = 'https://vue-gist.firebaseio.com/data.json';
 new Vue({
     el: '#app',
     router,
-    render: h => h(App)
+    render: h => h(App),
+    created() {
+      firebase.initializeApp({
+        apiKey: 'AIzaSyAZ49TkEWbxwo7K7bI99kBsPpWklyp-cdI',
+        authDomain: 'vue-gist.firebaseapp.com',
+        databaseURL: 'https://vue-gist.firebaseio.com',
+        projectId: 'vue-gist',
+        storageBucket: 'vue-gist.appspot.com',
+        messagingSenderId: '943547530323',
+        appId: '1:943547530323:web:93fc78af4170fa83'
+      })
+    },
 })
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,41 @@
+import Router from 'vue-router';
+import Vue from 'vue';
+import Login from './components/Login.vue';
+import SignUp from './components/SignUp.vue';
 import NewGist from './components/NewGist.vue';
-export const routes = [{
-    path: '',
-    component: NewGist
-  },
-];
+
+Vue.use(Router)
+
+export const routes = [
+      {
+        path:'',
+        component: Login
+      },
+      {
+        path: '/login',
+        name: 'login',
+        component: Login
+      },
+      {
+        path: '/sign-up',
+        name: 'signup',
+        component: SignUp
+      },
+      {
+        path: '/allgists',
+        name: 'newGist',
+        component: NewGist,
+        meta: {
+          requiresAuth: true
+        }
+      },
+    ];
+
+// router.beforeEach((to, from, next) => {
+//   let currentUser = firebase.auth().currentUser
+//   let requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+
+//   if (requiresAuth && !currentUser) next('/login')
+//   else if (!requiresAuth && currentUser) next('/allgists')
+//   else next()
+// })
